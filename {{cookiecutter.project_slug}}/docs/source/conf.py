443bcd4208ad8eaa8d527e4fd2284da29eaba70d
bwoodsend <bwoodsend@gmail.com>
2020-09-22 15:19:25 +0100

    Switch docs theme to sphinx-rdt-theme.

--- {{cookiecutter.project_slug}}/docs/source/conf.py
+++ {{cookiecutter.project_slug}}/docs/source/conf.py
@@ -31,7 +31,11 @@ import {{ cookiecutter.project_slug }}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx_copybutton',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -82,20 +86,17 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
 
-# Theme options are theme-specific and customize the look and feel of a
-# theme further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+#html_favicon = 'favicon.png'
 
 # -- Options for HTMLHelp output ---------------------------------------
 
@@ -157,6 +158,4 @@ texinfo_documents = [
      'One line description of project.',
      'Miscellaneous'),
 ]
-
-
 

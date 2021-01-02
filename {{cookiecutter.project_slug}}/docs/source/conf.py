917cd0c380dcb4d0aed273a3e79fa39a2543f962
bwoodsend <bwoodsend@gmail.com>
2021-01-02 12:00:33 +0000

    Add docs theme overides CSS.

--- {{cookiecutter.project_slug}}/docs/source/conf.py
+++ {{cookiecutter.project_slug}}/docs/source/conf.py
@@ -98,7 +98,10 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
+
+def setup(app):
+    app.add_css_file('theme-overrides.css')
 
 #html_favicon = 'favicon.png'
 
